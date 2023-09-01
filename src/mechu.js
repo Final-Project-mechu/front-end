9ad5a65a4bb73c3c8def2db49ed6a528d2dabd4c
Lee SeoWon <rymil9303@gmail.com>
2023-09-01 17:37:25 +0900

    fix: 변경된 사항 적용
    세령님, 하영님, 나(서원) 작업 합병

--- src/mechu.js
+++ src/mechu.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const filterButtons = document.querySelectorAll('.filter-button');
-  const subFilterButtons = document.querySelectorAll('.sub-filter-button');
-  const foodItems = document.querySelectorAll('.food-item');
-  const mainContent = document.querySelector('main');
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll(".filter-button");
+  const subFilterButtons = document.querySelectorAll(".sub-filter-button");
+  const foodItems = document.querySelectorAll(".food-item");
+  const mainContent = document.querySelector("main");
 
   // 처음에는 "식사" 버튼을 선택한 상태로 시작
   // filterButtons.forEach(button => {
@@ -14,111 +14,111 @@ document.addEventListener('DOMContentLoaded', function () {
   // });
 
   // 처음에는 "식사"와 "디저트"만 노출
-  subFilterButtons.forEach(subButton => {
-    const subCategory = subButton.getAttribute('data-category');
+  subFilterButtons.forEach((subButton) => {
+    const subCategory = subButton.getAttribute("data-category");
     if (
-      subCategory === 'korean' ||
-      subCategory === 'chinese' ||
-      subCategory === 'japanese' ||
-      subCategory === 'western' ||
-      subCategory === 'indian' ||
-      subCategory === 'coffee' ||
-      subCategory === 'cake' ||
-      subCategory === 'bread' ||
-      subCategory === 'tanghulu' ||
-      subCategory === 'icecream'
+      subCategory === "korean" ||
+      subCategory === "chinese" ||
+      subCategory === "japanese" ||
+      subCategory === "western" ||
+      subCategory === "indian" ||
+      subCategory === "coffee" ||
+      subCategory === "cake" ||
+      subCategory === "bread" ||
+      subCategory === "tanghulu" ||
+      subCategory === "icecream"
     ) {
-      subButton.style.display = 'none';
+      subButton.style.display = "none";
     } else {
-      subButton.style.display = 'none';
+      subButton.style.display = "none";
     }
   });
 
   // 처음에는 메인 내용 숨기기
-  mainContent.style.display = 'none';
+  mainContent.style.display = "none";
 
-  filterButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const category = button.getAttribute('data-category');
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const category = button.getAttribute("data-category");
 
       // 선택된 버튼에 "active" 클래스 추가
-      filterButtons.forEach(btn => {
-        btn.classList.remove('active');
+      filterButtons.forEach((btn) => {
+        btn.classList.remove("active");
       });
-      button.classList.add('active');
+      button.classList.add("active");
 
       // 메인 내용 표시
-      mainContent.style.display = 'block';
+      mainContent.style.display = "block";
 
-      foodItems.forEach(item => {
-        if (category === 'all' || item.classList.contains(category)) {
-          item.style.display = 'block';
+      foodItems.forEach((item) => {
+        if (category === "all" || item.classList.contains(category)) {
+          item.style.display = "block";
         } else {
-          item.style.display = 'none';
+          item.style.display = "none";
         }
       });
 
       // "식사" 버튼 클릭 시
-      if (category === 'meal') {
-        subFilterButtons.forEach(subButton => {
-          const subCategory = subButton.getAttribute('data-category');
+      if (category === "meal") {
+        subFilterButtons.forEach((subButton) => {
+          const subCategory = subButton.getAttribute("data-category");
           if (
-            subCategory === 'all-meal' ||
-            subCategory === 'korean' ||
-            subCategory === 'chinese' ||
-            subCategory === 'japanese' ||
-            subCategory === 'western' ||
-            subCategory === 'indian'
+            subCategory === "all-meal" ||
+            subCategory === "korean" ||
+            subCategory === "chinese" ||
+            subCategory === "japanese" ||
+            subCategory === "western" ||
+            subCategory === "indian"
           ) {
-            subButton.style.display = 'block';
+            subButton.style.display = "block";
           } else {
-            subButton.style.display = 'none';
+            subButton.style.display = "none";
           }
         });
       }
       // "디저트" 버튼 클릭 시
-      else if (category === 'dessert') {
-        subFilterButtons.forEach(subButton => {
-          const subCategory = subButton.getAttribute('data-category');
+      else if (category === "dessert") {
+        subFilterButtons.forEach((subButton) => {
+          const subCategory = subButton.getAttribute("data-category");
           if (
-            subCategory === 'coffee' ||
-            subCategory === 'cake' ||
-            subCategory === 'bread' ||
-            subCategory === 'tanghulu' ||
-            subCategory === 'icecream'
+            subCategory === "coffee" ||
+            subCategory === "cake" ||
+            subCategory === "bread" ||
+            subCategory === "tanghulu" ||
+            subCategory === "icecream"
           ) {
-            subButton.style.display = 'block';
+            subButton.style.display = "block";
           } else {
-            subButton.style.display = 'none';
+            subButton.style.display = "none";
           }
         });
       }
       // 다른 카테고리 버튼 클릭 시
       else {
-        subFilterButtons.forEach(subButton => {
-          subButton.style.display = 'none';
+        subFilterButtons.forEach((subButton) => {
+          subButton.style.display = "none";
         });
       }
     });
   });
 
-  subFilterButtons.forEach(subButton => {
-    subButton.addEventListener('click', () => {
-      const subCategory = subButton.getAttribute('data-category');
+  subFilterButtons.forEach((subButton) => {
+    subButton.addEventListener("click", () => {
+      const subCategory = subButton.getAttribute("data-category");
 
-      foodItems.forEach(item => {
-        const mainCategory = item.classList.contains('meal')
-          ? 'meal'
-          : 'dessert';
+      foodItems.forEach((item) => {
+        const mainCategory = item.classList.contains("meal")
+          ? "meal"
+          : "dessert";
 
         if (
-          subCategory === 'all' ||
+          subCategory === "all" ||
           item.classList.contains(subCategory) ||
           subCategory === mainCategory
         ) {
-          item.style.display = 'block';
+          item.style.display = "block";
         } else {
-          item.style.display = 'none';
+          item.style.display = "none";
         }
       });
     });
